75ceb34e5ecb51457c853de7b119f72303707099
laya-resort-hotel <layaresorthotel@gmail.com>
2026-04-01 09:23:02 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'laya-mod-v28';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './style.css',
+  './manifest.webmanifest',
+  './assets/logo.png',
+  './assets/icons/icon-192.png',
+  './assets/icons/icon-512.png',
+  './assets/icons/apple-touch-icon.png',
+  './assets/icons/maskable-icon-512.png',
+  './data/checklist_templates.json',
+  './js/app.js',
+  './js/firebase-config.js',
+  './js/firebase-init.js',
+  './mod_service_layer.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      if (cached) return cached;
+      return fetch(req).then((resp) => {
+        const copy = resp.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          if (req.url.startsWith(self.location.origin)) cache.put(req, copy).catch(() => {});
+        });
+        return resp;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
